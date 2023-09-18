66fc9012478e142fd4dfa3d50cea71da4973cb7a
dymonyx <lapshina.study@mail.ru>
2023-09-19 02:33:21 +0300

    add new task1

--- Lab1/task1.py
+++ Lab1/task1.py
@@ -1,19 +1,19 @@
-def binary_search(a: list[float], x: float) -> int:
-    iter_count = 0
-    left_bound, right_bound = 0, len(a) - 1
-    while left_bound <= right_bound:
-        mid = left_bound + (right_bound - left_bound) // 2
-        iter_count += 1
-        if a[mid] == x:
-            return iter_count
-        if a[mid] < x:
-            left_bound = mid + 1
+def binary_search(arr, number):
+    arr, number = sorted(arr), float(number)
+    steps = 0
+    low, high  = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        steps += 1
+        if arr[mid] == number:
+            return steps
+        elif arr[mid] < number:
+            low = mid + 1
         else:
-            right_bound = mid - 1
-    return -1
+            high = mid - 1
+    return "введённого числа нет в заданном списке"
 
-
-if __name__ == "__main__":
-    a = list(map(float, input().split()))
-    x = float(input())
-    print(binary_search(a, x))
+if __name__ == '__main__':
+    arr = list(map(float, input('введите список: ').split()))
+    number = input('введите число: ')
+    print(binary_search(arr, number))
