d8b86874ec999e0b7cd1acbcae72e0a3e56b2b8a
dymonyx <lapshina.study@mail.ru>
2023-10-02 01:09:40 +0300

    fix some issues with task2

--- Lab2/task2.py
+++ Lab2/task2.py
@@ -1,10 +1,10 @@
 def sort_bubble(arr):
-    for i in range(len(arr) - 1):
-        for j in range(i + 1, len(arr) - 1):
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
             if arr[i] < arr[j]:
                 arr[i], arr[j] = arr[j], arr[i]
     return arr
 
 if __name__ == '__main__':
-    arr = map(float, input().split())
+    arr = list(map(float, input().split()))
     print(sort_bubble(arr))
