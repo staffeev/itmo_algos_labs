6433e45b630f78b03be2b3292e26e62817ab36fb
staffeev <st.affeev@yandex.ru>
2023-10-02 09:32:47 +0300

    task3 O(factorial(n)) completed

--- Lab2/task3.py
+++ Lab2/task3.py
@@ -4,11 +4,11 @@ def _make_vector(p1,  p2):
     return _x, _y
 
 
-def o_n_cube(n: int):
+def o_n_cube(n: int) -> int:
     """Пусть есть n точек на координатной плоскости. Сколько существует
     троек точек (p1, p2, p3), что сумма векторов, образованных точками 
     (p1, p2) и (p2, p3) соответственно (где 1-я точка - начало вектора, 2-я - конец)
-    является вектором, образующим тупой угол с положительным направлением оси Ox"""
+    является вектором, образующим тупой угол с положительным направлением оси Ox?"""
 
     from math import atan2, degrees
     points = []
@@ -27,6 +27,32 @@ def o_n_cube(n: int):
     return count
 
 
+def o_n_fact(n: int) -> int:
+    """Сколько существует перестановок длины n таких, что XOR первого и последнего элемента
+    больше XOR остальных элементов"""
+
+    from itertools import permutations
+
+    count = 0
+    p = list(permutations(range(1, n + 1)))
+
+    if n % 4 == 0:
+        s = n
+    elif n % 4 == 1:
+        s = 1
+    elif n % 4 == 2:
+        s = n + 1
+    elif n % 4 == 3:
+        s = 0
+
+    for i in p:
+        v = i[0] ^ i[-1]
+        if v > s ^ v:
+            count += 1
+    return count
+
+
 if __name__ == "__main__":
-    print(o_n_cube(int(input())))
+    # print(o_n_cube(int(input())))
+    print(o_n_fact(int(input())))
     
