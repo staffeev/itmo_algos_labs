14cceb1e90605ef730d20abe750ba9870bb1f9fa
dymonyx <lapshina.study@mail.ru>
2023-11-12 16:50:34 +0300

    task1 УРААА

--- Lab5/task1.py
+++ Lab5/task1.py
@@ -5,6 +5,7 @@ def knapsack_dynamic_programming(stuff, capacity):
     n = len(stuff)
     dp = [[0] * (capacity + 1) for _ in range(n + 1)]
 
+    #заполнение dp динамически
     for i in range(1, n + 1):
         for j in range(capacity + 1):
             weight, price = stuff[i - 1]
@@ -13,30 +14,30 @@ def knapsack_dynamic_programming(stuff, capacity):
                 dp[i][j] = max(dp[i][j], dp[i - 1][j - weight] + price)
 
 
-
+    #получение забранных предметов
     result = []
-    w, v = capacity, dp[n][capacity]
 
     for i in range(n, 0, -1):
-        if dp[i][w] != dp[i - 1][w]:
+        if dp[i][capacity] != dp[i - 1][capacity]:
                 result.append(stuff[i - 1])
-                w -= stuff[i - 1][0]
+                capacity -= stuff[i - 1][0]
+
     return result
 
 
 
 if __name__ == '__main__':
     N, M, K = map(int, input().split())
-    stuff = [(random.randint(1, 30), random.randint(1, 30)) for _ in range(50)]
+    """stuff = [(random.randint(1, 30), random.randint(1, 30)) for _ in range(N)]"""
+    stuff = [(1, 2), (2, 1), (1, 4), (2, 10), (1, 14), (6, 10), (3, 8)]
     print(stuff)
     general_stolen_stuff = []
     for _ in range(M):
-        current_stuff = copy.deepcopy(stuff)
-        current_capacity = min(K, len(current_stuff))
-        # Ограничиваем вместимость рюкзака для каждого захода
-        stolen_stuff = knapsack_dynamic_programming(current_stuff, current_capacity)
-        general_stolen_stuff.append(stolen_stuff)
+        if len(stuff) == 0:
+            break
 
+        stolen_stuff = knapsack_dynamic_programming(stuff, K)
+        general_stolen_stuff.append(stolen_stuff)
 
         # Удаляем вещи, которые унесли
         for item in stolen_stuff:
