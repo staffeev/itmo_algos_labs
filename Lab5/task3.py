d26590d90a37479eb7689a220808e45c5cf246e7
staffeev <st.affeev@yandex.ru>
2023-11-13 14:27:42 +0300

    microchanges

--- Lab5/task3.py
+++ Lab5/task3.py
@@ -1,6 +1,7 @@
 from random import randint
 import simple_colors as sc
 
+
 def find_longest_increasing_seq(arr):
     n = len(arr)
     dp = [1] * n
@@ -12,18 +13,14 @@ def find_longest_increasing_seq(arr):
     max_length = max(dp)
     max_length_index = dp.index(max_length)
     start_index = max_length_index - max_length + 1
-    subseq = arr[start_index:max_length_index + 1]
-    return subseq, len(subseq)
+    return start_index, max_length_index
 
 
 if __name__ == "__main__":
-    arr = [5, 3, 8, 1, 9, 2, 4, 7]
-    seq, length = find_longest_increasing_seq(arr)
-    print(seq)  # [2, 4, 7]
-    print(length)  # 3
-
-    # n = int(input())
-    # array = [randint(-100, 100) for _ in range(n)]
-    # print(*array)
-    # start_ix, end_ix = find_subseq(n, array)
-    # print(*[sc.cyan(array[i]) if start_ix <= i <= end_ix else array[i] for i in range(n)])
+    n = int(input())
+    array = [randint(-100, 100) for _ in range(n)]
+    print("Исходный массив")
+    print(*array)
+    start_ix, end_ix = find_longest_increasing_seq(array)
+    print("С выделенной подпоследовательностью:")
+    print(*[sc.cyan(array[i]) if start_ix <= i <= end_ix else array[i] for i in range(n)])
