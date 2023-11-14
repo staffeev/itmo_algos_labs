45b4155c1823bc6fe171b9a7f16519b6b08cabd9
dymonyx <lapshina.study@mail.ru>
2023-11-14 04:00:58 +0300

    new name of function

--- Lab5/task1.py
+++ Lab5/task1.py
@@ -1,13 +1,13 @@
 import random
 import copy
 
-def knapsack_dynamic_programming(stuff, capacity):
+def knapsack(stuff, capacity):
     n = len(stuff)
     dp = [[0] * (capacity + 1) for _ in range(n + 1)]
 
     #заполнение dp динамически
-    for i in range(1, n + 1):
-        for j in range(capacity + 1):
+    for i in range(1, n + 1): #для каждого предмета
+        for j in range(capacity + 1): #осуществляем перебор всех вместимостей рюкзака
             weight, price = stuff[i - 1]
             dp[i][j] = dp[i - 1][j]
             if weight <= j:
@@ -28,22 +28,23 @@ def knapsack_dynamic_programming(stuff, capacity):
 
 if __name__ == '__main__':
     N, M, K = map(int, input().split())
-    """stuff = [(random.randint(1, 30), random.randint(1, 30)) for _ in range(N)]"""
-    stuff = [(1, 2), (2, 1), (1, 4), (2, 10), (1, 14), (6, 10), (3, 8)]
+    stuff = [(random.randint(1, 30), random.randint(1, 30)) for _ in range(N)]
+    """stuff = [(1, 2), (2, 1), (1, 4), (2, 10), (1, 14), (6, 10), (3, 8)]"""
     print(stuff)
     general_stolen_stuff = []
     for _ in range(M):
         if len(stuff) == 0:
             break
 
-        stolen_stuff = knapsack_dynamic_programming(stuff, K)
+
+        stolen_stuff = knapsack(stuff, K)
         general_stolen_stuff.append(stolen_stuff)
 
         # Удаляем вещи, которые унесли
         for item in stolen_stuff:
             stuff.remove(item)
 
-
+    print(general_stolen_stuff)
 
     for i, stolen_stuff in enumerate(general_stolen_stuff, start=1):
         print(f"Заход {i}:")
